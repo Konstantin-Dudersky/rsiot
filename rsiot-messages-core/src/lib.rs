22151fe8c76a0756d6a42d60d97e25e27357640c
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2023-11-03 18:39:15 +0300

    messages-core key

--- rsiot-messages-core/src/lib.rs
+++ rsiot-messages-core/src/lib.rs
@@ -4,4 +4,14 @@ pub trait IMessage
 where
     Self: Clone + Debug + Send,
 {
+    /// Ключ для сохранения в базе данных
+    fn key(&self) -> String {
+        let full_str = format!("{:?}", self);
+        let parenth_index = full_str.find('(');
+        let full_str: String = match parenth_index {
+            Some(value) => full_str.chars().take(value).collect(),
+            None => full_str,
+        };
+        full_str
+    }
 }
