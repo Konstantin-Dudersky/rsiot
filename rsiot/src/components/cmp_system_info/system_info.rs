a3d899ed8cc4dcc4fd1d74d46c7e919cf36c3e7b
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-03-21 17:29:37 +0300

    raspberry

--- rsiot/src/components/cmp_system_info/system_info.rs
+++ rsiot/src/components/cmp_system_info/system_info.rs
@@ -11,6 +11,18 @@ pub struct SystemInfo {
 
     /// Информация о сетевых подключениях
     pub networks: HashMap<String, SystemInfoNetwork>,
+
+    /// Returns CPU’s usage.
+    pub cpu_usage: Vec<f32>,
+
+    /// Temperature of components
+    pub temperatures: HashMap<String, f32>,
+
+    /// Информация о памяти
+    pub memory: SystemInfoMemory,
+
+    /// Информация о дисках
+    pub disks: HashMap<String, SystemInfoDisk>,
 }
 
 /// Информация о сетевом подключении
@@ -21,4 +33,33 @@ pub struct SystemInfoNetwork {
 
     /// MAC address for network interface
     pub mac_address: String,
+}
+
+/// Информация о потребленной памяти
+#[derive(Debug, Default)]
+pub struct SystemInfoMemory {
+    /// Доступный размер оперативной памяти, [MB]
+    pub total_memory_mb: f32,
+
+    /// Используемый размер оперативной памяти, [MB]
+    pub used_memory_mb: f32,
+
+    /// Общий размер swap, [MB]
+    pub total_swap_mb: f32,
+
+    /// Используемый размер swap, [MB]
+    pub used_swap_mb: f32,
+}
+
+/// Информация о диске
+#[derive(Debug, Default)]
+pub struct SystemInfoDisk {
+    /// Название
+    pub name: String,
+
+    /// Занятое пространство, [GB]
+    pub used_space_gb: f32,
+
+    /// Общее пространство, [GB]
+    pub total_space_gb: f32,
 }
