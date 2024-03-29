d08a9fe1175679eb15d86904d3de0190639bf346
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-03-29 09:54:02 +0300

    esp logging

--- rsiot/src/logging/target_esp.rs
+++ rsiot/src/logging/target_esp.rs
@@ -3,6 +3,19 @@ use log::LevelFilter as LogLevelFilter;
 use tracing::level_filters::LevelFilter as TracingLevelFilter;
 
 /// Конфигурация логгирования для микроконтроллера ESP32
+///
+/// Также необходимо настроить переменную CONFIG_LOG_DEFAULT_LEVEL_ в sdkconfig.defaults. Возможные
+/// значения:
+/// - No output (CONFIG_LOG_DEFAULT_LEVEL_NONE)
+/// - Error (CONFIG_LOG_DEFAULT_LEVEL_ERROR)
+/// - Warning (CONFIG_LOG_DEFAULT_LEVEL_WARN)
+/// - Info (CONFIG_LOG_DEFAULT_LEVEL_INFO)
+/// - Debug (CONFIG_LOG_DEFAULT_LEVEL_DEBUG)
+/// - Verbose (CONFIG_LOG_DEFAULT_LEVEL_VERBOSE)
+///
+/// В консоль будут попадать логи, которые являются минимумом двух значений:
+/// - заданного в файле `sdkconfig.defaults` при компиляции
+/// - значения аргумента `level` данной функции в рантайме
 pub fn configure_logging(level: TracingLevelFilter) -> super::Result<()> {
     EspLogger::initialize_default();
 
@@ -14,6 +27,6 @@ pub fn configure_logging(level: TracingLevelFilter) -> super::Result<()> {
         TracingLevelFilter::ERROR => LogLevelFilter::Error,
         TracingLevelFilter::OFF => LogLevelFilter::Off,
     };
-    set_target_level("", level)?;
+    set_target_level("*", level)?;
     Ok(())
 }
