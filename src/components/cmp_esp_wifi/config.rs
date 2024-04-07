18a231e6a9d3ba72dbadb83e2cdf20f4f6109a24
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-04-07 09:57:02 +0300

    cmp_esp_wifi fix

--- src/components/cmp_esp_wifi/config.rs
+++ src/components/cmp_esp_wifi/config.rs
@@ -1,9 +1,39 @@
+pub use esp_idf_svc::wifi::AuthMethod as ConfigAuthMethod;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::modem::Modem};
 
 /// Конфигурация cmp_esp_wifi
 pub struct Config {
     /// Модем для подключения
     pub peripherals: Modem,
+
     /// Ссылка на цикл событий
     pub event_loop: EspSystemEventLoop,
+
+    /// Настройка WiFi как точки доступа
+    ///
+    /// None - точка доступа не настраиваeтся
+    pub access_point: Option<ConfigAccessPoint>,
+
+    /// Настройка WiFi как клиента
+    ///
+    /// None - подключение к внешней точке доступа не настраивается
+    pub client: Option<ConfigClient>,
+}
+
+/// Настройка WiFi как точки доступа
+pub struct ConfigAccessPoint {
+    /// Название точки доступа
+    pub ssid: String,
+}
+
+/// Настройка WiFi как клиента
+pub struct ConfigClient {
+    /// Название точки доступа
+    pub ssid: String,
+
+    /// Пароль точки доступа
+    pub password: String,
+
+    /// Защита сети
+    pub auth_method: ConfigAuthMethod,
 }
--- src/components/cmp_esp_wifi/fn_process.rs
+++ src/components/cmp_esp_wifi/fn_process.rs
@@ -1,7 +1,11 @@
 use std::time::Duration;
 
-use esp_idf_svc::wifi::EspWifi;
+use esp_idf_svc::{
+    eventloop::{EspEventLoop, System},
+    wifi::{BlockingWifi, ClientConfiguration, Configuration, EspWifi},
+};
 use tokio::time::sleep;
+use tracing::info;
 
 use crate::{executor::CmpInOut, message::MsgDataBound};
 
@@ -11,44 +15,55 @@ pub async fn fn_process<TMsg>(config: Config, _in_out: CmpInOut<TMsg>) -> super:
 where
     TMsg: MsgDataBound,
 {
+    let wifi_config = prepare_wifi_config(&config);
+
     let mut driver = EspWifi::new(config.peripherals, config.event_loop.clone(), None).unwrap();
 
-    wifi_setup(
-        &mut driver,
-        config.event_loop,
-        // Configuration::Mixed(
-        //     ClientConfiguration {
-        //         ssid: "Fermenter".into(),
-        //         password: "k33n3+Ik".into(),
-        //         auth_method: AuthMethod::None,
-
-        //         ..Default::default()
-        //     },
-        //     AccessPointConfiguration {
-        //         ssid: "test_esp_ap".into(),
-        //         ..Default::default()
-        //     },
-        // ),
-        Configuration::AccessPoint(esp_idf_svc::wifi::AccessPointConfiguration {
-            ssid: heapless::String::try_from("test_esp").unwrap(),
-            ..Default::default()
-        }),
-    );
+    wifi_setup(&mut driver, config.event_loop, wifi_config);
 
     loop {
         sleep(Duration::from_secs(2)).await;
     }
 }
 
-use esp_idf_svc::{
-    eventloop::{EspEventLoop, System},
-    wifi::{BlockingWifi, Configuration},
-};
-use tracing::info;
+fn prepare_wifi_config(config: &Config) -> Configuration {
+    let access_point_config = match &config.access_point {
+        Some(ap) => Some(esp_idf_svc::wifi::AccessPointConfiguration {
+            ssid: heapless::String::try_from(ap.ssid.as_str()).unwrap(),
+            ..Default::default()
+        }),
+        None => None,
+    };
+
+    let client_config: Option<ClientConfiguration> = match &config.client {
+        Some(cl) => Some(ClientConfiguration {
+            ssid: heapless::String::try_from(cl.ssid.as_str()).unwrap(),
+            password: heapless::String::try_from(cl.password.as_str()).unwrap(),
+            auth_method: cl.auth_method,
+            ..Default::default()
+        }),
+        None => None,
+    };
+
+    if access_point_config.is_some() && client_config.is_some() {
+        Configuration::Mixed(
+            client_config.expect("Already checked"),
+            access_point_config.expect("Already checked"),
+        )
+    } else if access_point_config.is_some() {
+        Configuration::AccessPoint(access_point_config.expect("Already checked"))
+    } else if client_config.is_some() {
+        Configuration::Client(client_config.expect("Already checked"))
+    } else {
+        todo!()
+    }
+}
 
 /// Запустить Wi-Fi в комбинированном режиме.
 ///
-/// TODO - обработка ошибок, перезапуск
+/// TODO - обработка ошибок
+/// TODO - перезапуск
+/// TODO - AsyncWifi
 pub fn wifi_setup(
     wifi: &mut EspWifi<'static>,
     sys_loop: EspEventLoop<System>,
@@ -65,7 +80,7 @@ pub fn wifi_setup(
         || matches!(configuration, Configuration::Mixed(_, _))
     {
         wifi.connect().unwrap();
-        info!("Wifi connected");
+        info!("Wifi connected to external AP");
         wifi.wait_netif_up().unwrap();
         info!("Wifi netif up");
         let ip_info = wifi.wifi().sta_netif().get_ip_info().unwrap();
