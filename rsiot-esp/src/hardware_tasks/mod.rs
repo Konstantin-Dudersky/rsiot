c6894f6ae6998165f28f11ba33bb057c0e147095
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-04-16 19:15:34 +0300

    delete rsiot-esp

--- rsiot-esp/src/hardware_tasks/mod.rs
+++
@@ -1,7 +0,0 @@
-mod gpio_input;
-mod gpio_output;
-mod wifi_setup;
-
-pub use gpio_input::*;
-pub use gpio_output::*;
-pub use wifi_setup::*;
--- src/components/cmp_raspberrypi_gpio/mod.rs
+++ src/components/cmp_raspberrypi_gpio/mod.rs
@@ -1,13 +1,4 @@
 //! Компонент для работы с GPIO Raspberry Pi
-//!
-//! Обертка над библиотекой [rppal](https://crates.io/crates/rppal)
-//!
-//! Запускать программу скорее всего нужно из-под sudo.
-//!
-//! Распиновка -
-//! https://www.raspberrypi.com/documentation/computers/os.html#gpio-and-the-40-pin-header
-//!
-//!
 
 mod component;
 mod config;
