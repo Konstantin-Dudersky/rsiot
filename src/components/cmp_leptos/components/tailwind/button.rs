8658e6b3f2c1548bf1860a2a476f8affee4354a8
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-05-18 15:12:33 +0300

    cmp_leptos

---
+++ src/components/cmp_leptos/components/tailwind/button.rs
@@ -0,0 +1,22 @@
+use leptos::*;
+
+#[component]
+pub fn Button<FClicked>(
+    /// Текст кнопки
+    #[prop(default = "Button text")]
+    text: &'static str,
+
+    /// Событие нажатия
+    // clicked: WriteSignal<()>,
+    clicked: FClicked,
+) -> impl IntoView
+where
+    FClicked: Fn() -> () + 'static,
+{
+    view! {
+        <button type="button" class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        on:click= move |_| (clicked)()
+        >{ text }</button>
+
+    }
+}
--- src/components/cmp_leptos/components/tailwind/mod.rs
+++ src/components/cmp_leptos/components/tailwind/mod.rs
@@ -1,3 +1,5 @@
+mod button;
 mod simple_toggle;
 
+pub use button::Button;
 pub use simple_toggle::SimpleToggle;
