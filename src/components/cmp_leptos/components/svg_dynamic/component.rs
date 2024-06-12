49e4cbbf499218e1125f0be8dcf152bbf3173a8b
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-06-12 17:23:17 +0300

    svg_dynamic

--- src/components/cmp_leptos/components/svg_dynamic/component.rs
+++ src/components/cmp_leptos/components/svg_dynamic/component.rs
@@ -1,20 +1,41 @@
-use leptos::*;
-use tracing::warn;
-use wasm_bindgen::{JsCast, JsValue};
+use std::sync::{Arc, Mutex};
 
-use super::{svg_input::SvgInputSignal, SvgInput};
+use ev::Event;
+use leptos::*;
+use tracing::{info, warn};
+use wasm_bindgen::{closure::Closure, JsCast, JsValue};
+
+use super::{svg_input::SvgInputSignal, SvgInput, SvgOutput};
 
 #[component]
-pub fn SvgDynamic(file: &'static str, svg_input: Vec<SvgInput>) -> impl IntoView {
+pub fn SvgDynamic<FOutput>(
+    file: &'static str,
+    svg_input: Vec<SvgInput>,
+    svg_output: SvgOutput<FOutput>,
+) -> impl IntoView
+where
+    FOutput: Fn(&str) -> () + 'static,
+{
     let div_ref = create_node_ref();
+
+    let output_callback = Arc::new(Mutex::new(Closure::wrap(Box::new(move |e: Event| {
+        let s = e.to_string().as_string().unwrap();
+        let e1 = e.target().unwrap();
+
+        info!("{:?}", e.target().unwrap());
+        (svg_output.callback)(&s)
+    }) as Box<dyn FnMut(_)>)));
+
     div_ref.on_load(move |_| {
         for input in svg_input {
+            let cb_clone = output_callback.clone();
+
             create_effect(move |_| {
                 let element = document().get_element_by_id(&input.id);
                 let element = match element {
                     Some(val) => val,
                     None => {
-                        warn!("Element with id {} not found", input.id);
+                        warn!("Element with id '{}' not found", input.id);
                         return;
                     }
                 };
@@ -23,15 +44,54 @@ pub fn SvgDynamic(file: &'static str, svg_input: Vec<SvgInput>) -> impl IntoView
                 let svg_element = match svg_element {
                     Ok(val) => val,
                     Err(_) => {
-                        warn!("Cannot cast element with id {} into SvgElement", input.id);
+                        warn!("Cannot cast element with id '{} 'into SvgElement", input.id);
                         return;
                     }
                 };
 
                 let result = change_svg_element(&input, &svg_element);
                 if let Err(err) = result {
-                    warn!("Cannot set attribute TODO of svg element with id {err:?}")
+                    warn!(
+                        "Cannot set attribute TODO of svg element with id '{}' :{:?}",
+                        input.id, err
+                    )
                 };
+
+                let lock = cb_clone.lock().unwrap();
+                svg_element
+                    .add_event_listener_with_callback("click", &lock.as_ref().unchecked_ref())
+                    .unwrap();
+            });
+        }
+
+        for id in svg_output.ids {
+            let cb_clone = output_callback.clone();
+            // Не знаю, зачем нужно оборачивать в create_effect, но без него `get_element_by_id` не
+            // находится
+            create_effect(move |_| {
+                let element = document().get_element_by_id(&id);
+                let element = match element {
+                    Some(val) => val,
+                    None => {
+                        warn!("Element with id '{}' not found", id);
+                        return;
+                    }
+                };
+
+                let svg_element = element.dyn_into::<web_sys::SvgElement>();
+                let svg_element = match svg_element {
+                    Ok(val) => val,
+                    Err(_) => {
+                        warn!("Cannot cast element with id '{}' into SvgElement", id);
+                        return;
+                    }
+                };
+
+                let lock = cb_clone.lock().unwrap();
+
+                svg_element
+                    .add_event_listener_with_callback("click", &lock.as_ref().unchecked_ref())
+                    .unwrap();
             });
         }
     });
--- src/components/cmp_leptos/components/svg_dynamic/mod.rs
+++ src/components/cmp_leptos/components/svg_dynamic/mod.rs
@@ -3,6 +3,8 @@
 
 mod component;
 mod svg_input;
+mod svg_output;
 
 pub use component::SvgDynamic;
 pub use svg_input::SvgInput;
+pub use svg_output::SvgOutput;
