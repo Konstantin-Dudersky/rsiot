66d0a8fa35c5a0f485b2ebac0e5994311d326365
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2025-08-30 10:04:08 +0300

    remove cmp_http_client_old from components_config

--- src/components_config/http_client_old/types.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-
-use crate::message::Message;
-
-/// Коллбек успешного ответа
-pub type CbkOnSuccess<TMsg> = fn(&str) -> Result<Vec<Message<TMsg>>>;
-
-/// Коллбек неуспешного ответа
-pub type CbkOnFailure<TMsg> = fn() -> Vec<Message<TMsg>>;
